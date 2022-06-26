39a27035332df455db31939fe65f5dfe880f13e0
Jarrett <291979254@qq.com>
2022-06-26 19:32:13 +0800

    增加了文件上传的功能

--- src/views/newDocument/AddDoc.vue
+++ src/views/newDocument/AddDoc.vue
@@ -2,10 +2,10 @@
     <Upload
         multiple
         type="drag"
-        action="//jsonplaceholder.typicode.com/posts/">
+        action="http://localhost:8082/files/upload">
         <div style="padding: 20px 0">
             <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
-            <p>Click or drag files here to upload</p>
+            <p>点击上传</p>
         </div>
     </Upload>
 </template>
