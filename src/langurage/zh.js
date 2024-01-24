f72ac846c1064b0a0730574648fa99f20334a97b
Jarrett <291979254@qq.com>
2024-01-24 22:57:20 +0800

    add: vue-i18n

---
+++ src/langurage/zh.js
@@ -0,0 +1,15 @@
+export default{
+    table: {
+        username: '用户名',
+        email: '邮箱',
+        mobile: '手机'
+    },
+    menus: {
+        home: '首页',
+        download: '下载'
+    },
+    tabs: {
+        info: '商品描述',
+        comment: '评价'
+    }
+}
