2b4d84425ba848dc306d4c6696946d31bae98c8a
Jarrett <291979254@qq.com>
2024-03-04 22:45:35 +0800

    add: mp4 preview component

--- src/views/preview/VideoView.vue
+++ src/views/preview/VideoView.vue
@@ -5,28 +5,45 @@
 </template>
 
 <script>
-import 'video.js/dist/video-js.css'; // 引入样式
-import VideoPlayer from 'vue-video-player';
+// https://github.com/surmon-china/videojs-player/tree/vue-video-player
+// vue-video-player 只支持vue2
+// 参考地址：https://blog.csdn.net/qq_31455841/article/details/112497239
+// import { VueVideoPlayer } from 'vue-video-player';
+import 'video.js/dist/video-js.css'
+import { videoPlayer } from 'vue-video-player'
+import { BackendUrl } from '@/api/request'
 
 export default {
+    name: "VideoView",
     components: {
-        VideoPlayer,
+        videoPlayer
     },
     data() {
         return {
             videoOptions: {
                 autoplay: false,
                 controls: true,
+                height: 616,
                 sources: [
                     {
-                        src: 'https://example.com/your-video.mp4', // 视频文件的 URL
+                        src: 'http://81.69.247.172:8082/api/v1.0/files/view/65d9aa50e5186924ac5bfaac', // 视频文件的 URL
                         type: 'video/mp4',
                     },
                 ],
             },
+            src: ""
         };
     },
+    mounted() {
+        this.init()
+    },
     methods: {
+        init() {
+            let docId = this.$route.query.docId;
+            let wordURL = BackendUrl() + '/files/view/' + docId;
+            this.src = wordURL
+            this.videoOptions.sources[0].src = wordURL
+        },
         videoEnded() {
             console.log('视频播放结束');
         },
--- src/views/preview/index.vue
+++ src/views/preview/index.vue
@@ -31,12 +31,13 @@
                 </div>
             </div>
             <div class="doc-preview">
+                <video-view v-if="suffix === 'mp4'"></video-view>
                 <component :is="component"
-                           v-if="component"
+                           v-if="component && suffix !== 'mp4'"
                            :previewId="previewId"
                            :thumbId="thumbId"
                 />
-                <div class="preview-button" v-if="suffix=='pdf'">
+                <div class="preview-button" v-if="suffix==='pdf'">
                     <Button type="primary" @click="preview">全屏预览</Button>
                 </div>
             </div>
@@ -67,6 +68,8 @@ import {parseTime} from "@/utils/index"
 import DocOperation from "./docOperation"
 
 import CommentPage from "./CommentPage"
+// import VideoView from "./VideoView"
+import VideoView from "@/views/preview/VideoView";
 
 export default {
     data() {
@@ -90,6 +93,7 @@ export default {
         }
     },
     components: {
+        VideoView,
         Nav, DocOperation, CommentPage
     },
     mounted() {
@@ -157,6 +161,22 @@ export default {
                             break
                         case 'md':
                             this.component = () => import('@/views/preview/mdView')
+                            break
+                        case 'mp4':
+                            // this.component = () => import('@/views/preview/VideoView')
+                            // this.component = ({
+                            //     // 需要加载的组件 (应该是一个 `Promise` 对象)
+                            //     component: import('@/views/preview/VideoView'),
+                            //     // // 异步组件加载时使用的组件
+                            //     // loading: LoadingComponent,
+                            //     // // 加载失败时使用的组件
+                            //     // error: ErrorComponent,
+                            //     // 展示加载时组件的延时时间。默认值是 200 (毫秒)
+                            //     delay: 200,
+                            //     // 如果提供了超时时间且组件加载也超时了，
+                            //     // 则使用加载失败时使用的组件。默认值是：`Infinity`
+                            //     timeout: 3000
+                            // })
                             break
                         default:
                             this.component = () => import('@/views/preview/ErrorView')
